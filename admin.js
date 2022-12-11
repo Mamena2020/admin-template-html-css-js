a3adb3cd0a6d3374c6e5d3f56459d3b696f93f7f
mamena2020 <mamenaware751@gmail.com>
2022-12-12 01:24:08 +0900

    [1]. menu item collapsible

--- admin.js
+++ admin.js
@@ -22,3 +22,52 @@ function admin_close_topnav_menu() {
 function admin_logout() {
   console.log("logout")
 }
+
+
+// var coll = document.getElementsByClassName("the-list");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.height === "auto") {
+//       content.style.height = "0px";
+//         content.style.display = "none";
+//     } else {
+//       content.style.height = "auto";
+//         content.style.display = "block";
+//     }
+//   });
+// }
+
+
+
+// const option = document.getElementById("the-option-menu");
+// let text = collection.firstElementChild.innerHTML;
+// console.log(text)
+// document.getElementById("demo2").innerHTML = text;
+
+window.addEventListener("load",  ()=> {
+  var the_menu_collapsibles = document.getElementsByClassName("the-menu-collapsible");
+  var i
+  for (i = 0; i < the_menu_collapsibles.length; i++) {
+    the_menu_collapsibles[i].addEventListener("click", function () {
+      // var firstElement = this.firstElementChild;
+      this.childNodes.forEach(child => {
+        if (child.className === "the-menu-collapsible-list") {
+          if (child.style.height === "auto") {
+            child.style.height = 0;
+            child.style.opacity = 0;
+          } else {
+            child.style.height = 'auto';
+            child.style.opacity = 1;
+          }
+        }
+      });
+
+    });
+  }
+
+});
+
